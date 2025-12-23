c16cc1e2fc59932d9e01f81cd9bc15dba28786c2
Johny11sh <johnytayebbitar@gmail.com>
2025-12-23 17:26:47 +0300

    Symbol Table Visitor creation
    just the symbol table visitor is order to use the symbol table and label table and store nodes, entering and exiting scopes, adding new labels and these kind of stuff.
    visiting each node in the current grammar files we have ( in case we extended them or edit something in the future )

---
+++ CompilerPractice/src/visitors/SymbolTableVisitor.java
@@ -0,0 +1,263 @@
+package visitors;
+
+import ast.core.*;
+import ast.python.*;
+import table.*;
+
+import java.util.UUID;
+
+public class SymbolTableVisitor implements ASTVisitor<Void> {
+
+    private final SymbolTable symbolTable;
+    private final LabelTable labelTable;
+
+    public SymbolTableVisitor(SymbolTable symbolTable, LabelTable labelTable) {
+        this.symbolTable = symbolTable;
+        this.labelTable = labelTable;
+    }
+
+    // program
+
+    @Override
+    public Void visit(ProgramNode node) {
+        for (ASTNode child : node.getChildren()) {
+            child.accept(this);
+        }
+        return null;
+    }
+
+    // block
+
+    @Override
+    public Void visit(BlockNode node) {
+        for (ASTNode stmt : node.getChildren()) {
+            stmt.accept(this);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(StatementNode node) {
+        return null;
+    }
+
+    // functions
+
+    @Override
+    public Void visit(FunctionDefNode node) {
+
+        symbolTable.addSymbol(
+                node.getName(),
+                "function",
+                null,
+                node.getLine(),
+                node.getColumn()
+        );
+
+        symbolTable.enterScope(node.getName());
+
+        for (ParameterNode param : node.getParameters()) {
+            symbolTable.addSymbol(
+                    param.getName(),
+                    "parameter",
+                    null,
+                    param.getLine(),
+                    param.getColumn()
+            );
+        }
+
+        node.getBody().accept(this);
+
+        symbolTable.exitScope();
+        return null;
+    }
+
+    @Override
+    public Void visit(ParameterNode node) {
+        return null;
+    }
+
+    @Override
+    public Void visit(DecoratorNode node) {
+        return null;
+    }
+
+    // assignment
+
+    @Override
+    public Void visit(AssignmentNode node) {
+        IdentifierNode id = node.getTarget();
+
+        if (!symbolTable.existsInCurrentScope(id.getName())) {
+            symbolTable.addSymbol(
+                    id.getName(),
+                    "variable",
+                    null,
+                    id.getLine(),
+                    id.getColumn()
+            );
+        }
+
+        node.getValue().accept(this);
+        return null;
+    }
+
+    // if statements
+
+    @Override
+    public Void visit(IfNode node) {
+        labelTable.generateAnonymousLabel();
+
+        node.getCondition().accept(this);
+        node.getThenBlock().accept(this);
+
+        for (ElifNode e : node.getElifBlocks()) {
+            e.accept(this);
+        }
+
+        if (node.getElseBlock() != null) {
+            node.getElseBlock().accept(this);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Void visit(ElifNode node) {
+        labelTable.generateAnonymousLabel();
+        node.getCondition().accept(this);
+        node.getBlock().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(ElseNode node) {
+        labelTable.generateAnonymousLabel();
+        node.getBlock().accept(this);
+        return null;
+    }
+
+    // loops
+
+    @Override
+    public Void visit(WhileNode node) {
+        labelTable.generateAnonymousLabel();
+
+        node.getCondition().accept(this);
+        node.getBody().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(ForNode node) {
+
+        symbolTable.addSymbol(
+                node.getVariable().getName(),
+                "loop_var",
+                null,
+                node.getVariable().getLine(),
+                node.getVariable().getColumn()
+        );
+
+        labelTable.generateAnonymousLabel();
+
+        node.getIterable().accept(this);
+        node.getBody().accept(this);
+        return null;
+    }
+
+    // try and except blocks
+
+    @Override
+    public Void visit(TryNode node) {
+        labelTable.generateAnonymousLabel();
+
+        node.getTryBlock().accept(this);
+
+        for (ExceptNode e : node.getExceptBlocks()) {
+            e.accept(this);
+        }
+
+        if (node.getFinallyBlock() != null) {
+            node.getFinallyBlock().accept(this);
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(ExceptNode node) {
+        labelTable.generateAnonymousLabel();
+
+        if (node.getExceptionType() != null) {
+            node.getExceptionType().accept(this);
+        }
+
+        node.getBlock().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(FinallyNode node) {
+        labelTable.generateAnonymousLabel();
+        node.getBlock().accept(this);
+        return null;
+    }
+
+    // expressions
+
+    @Override
+    public Void visit(BinaryExpressionNode node) {
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(UnaryExpressionNode node) {
+        return null;
+    }
+
+    @Override
+    public Void visit(ComparisonNode node) {
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(LogicalExpressionNode node) {
+        return null;
+    }
+
+    @Override
+    public Void visit(CallExpressionNode node) {
+        node.getCallee().accept(this);
+        for (ExpressionNode arg : node.getArguments()) {
+            arg.accept(this);
+        }
+        return null;
+    }
+
+    @Override public Void visit(IdentifierNode node) { return null; }
+    @Override public Void visit(NumberLiteralNode node) { return null; }
+    @Override public Void visit(StringLiteralNode node) { return null; }
+    @Override public Void visit(BooleanLiteralNode node) { return null; }
+
+    @Override
+    public Void visit(NullLiteralNode node) {
+        return null;
+    }
+
+    @Override public Void visit(ListNode node) { return null; }
+    @Override public Void visit(DictNode node) { return null; }
+    @Override public Void visit(KeyValueNode node) { return null; }
+    @Override public Void visit(ReturnNode node) { return null; }
+
+    @Override
+    public Void visit(ExpressionNode node) {
+        return null;
+    }
+
+    @Override public Void visit(BreakNode node) { return null; }
+    @Override public Void visit(ContinueNode node) { return null; }
+}
